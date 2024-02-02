2276d99117c149edcf45f0de864adb3374cbb2d0
JptmJ <joshiptm797@gmail.com>
2024-02-02 09:43:57 +0530

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component: HomeComponent,
+        path: ''
+    },
+    {
+        component: ContactComponent,
+        path: 'contact'
+    },
+    {
+        component: AboutComponent,
+        path: 'about'
+    }
+];
